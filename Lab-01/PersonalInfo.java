98ce4ad2401fd803f31e362e2a90d6961bf30c5c
Samyog249 <ghimiresamyog249@gmail.com>
2026-07-18 17:03:19 +0545

    Personal Info

---
+++ Lab-01/PersonalInfo.java
@@ -0,0 +1,12 @@
+public class PersonalInfo {
+    public static void main(String[] args){
+        String name = "Samyog Ghimire";
+        int age = 19;
+        String address = "Bhaktapur,Nepal";
+        String college = "New Summit College";
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Address: "+address);
+        System.out.println("College: "+college);
+    }
+}
