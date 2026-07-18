1924f09fbdd39126c065997c392850d6310b5700
Samyog249 <ghimiresamyog249@gmail.com>
2026-07-18 17:15:43 +0545

    Execution process with javac and java command

---
+++ Lab-01/JVC.java
@@ -0,0 +1,6 @@
+public class JVC {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("This program is compiled with javac and run with java!");
+    }
+}
