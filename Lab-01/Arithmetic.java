50757313be740f5e8a71f5286f47b56ede99673e
Samyog249 <ghimiresamyog249@gmail.com>
2026-07-18 17:08:38 +0545

    Different Arithmetic Operations

---
+++ Lab-01/Arithmetic.java
@@ -0,0 +1,13 @@
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        int a = 40, b = 20;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+    }
+}
